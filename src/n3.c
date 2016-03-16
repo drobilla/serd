f7a89aedab8e282e05c0c2da89a04680ba8e7827
David Robillard <d@drobilla.net>
2016-03-16 16:21:20 -0400

    Use char* for strings in public API
    The constant casting just makes user code a mess, for no benefit.

--- src/n3.c
+++ src/n3.c
@@ -997,11 +997,10 @@ read_BLANK_NODE_LABEL(SerdReader* const reader,
   eat_byte_safe(reader, '_');
   eat_byte_check(reader, ':');
 
-  const Ref ref = *dest =
-    push_node(reader,
-              SERD_BLANK,
-              reader->bprefix ? (char*)reader->bprefix : "",
-              reader->bprefix_len);
+  const Ref ref = *dest = push_node(reader,
+                                    SERD_BLANK,
+                                    reader->bprefix ? reader->bprefix : "",
+                                    reader->bprefix_len);
 
   int c = peek_byte(reader); // First: (PN_CHARS | '_' | [0-9])
   if (is_digit(c) || c == '_') {
@@ -1040,7 +1039,6 @@ read_BLANK_NODE_LABEL(SerdReader* const reader,
       }
     }
   }
-
   return SERD_SUCCESS;
 }
 
@@ -1566,7 +1564,7 @@ tokcmp(SerdReader* const reader,
     return -1;
   }
 
-  return serd_strncasecmp((const char*)node->buf, tok, n);
+  return serd_strncasecmp(node->buf, tok, n);
 }
 
 SerdStatus
