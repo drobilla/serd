drobilla/serd

commit 443f470383dc3acba0fde7b705e8ff81a7c49595

Fix construction of URIs with UTF-8 characters

src/env.c
@@ -1,5 +1,5 @@
 /*
-  Copyright 2011-2014 David Robillard <http://drobilla.net>
+  Copyright 2011-2016 David Robillard <http://drobilla.net>
 
   Permission to use, copy, modify, and/or distribute this software for any
   purpose with or without fee is hereby granted, provided that the above
@@ -243,9 +243,9 @@ serd_env_expand_node(const SerdEnv*  env,
 		if (serd_env_expand(env, node, &prefix, &suffix)) {
 			return SERD_NODE_NULL;
 		}
-		const size_t len = prefix.len + suffix.len;  // FIXME: UTF-8?
+		const size_t len = prefix.len + suffix.len;
 		uint8_t*     buf = (uint8_t*)malloc(len + 1);
-		SerdNode     ret = { buf, len, len, 0, SERD_URI };
+		SerdNode     ret = { buf, len, serd_strlen(buf, NULL, NULL), 0, SERD_URI };
 		snprintf((char*)buf, ret.n_bytes + 1, "%s%s", prefix.buf, suffix.buf);
 		return ret;
 	}

COPYING
@@ -1,4 +1,4 @@
-Copyright 2011-2015 David Robillard <http://drobilla.net>
+Copyright 2011-2016 David Robillard <http://drobilla.net>
 
 Permission to use, copy, modify, and/or distribute this software for any
 purpose with or without fee is hereby granted, provided that the above

src/node.c
@@ -1,5 +1,5 @@
 /*
-  Copyright 2011-2015 David Robillard <http://drobilla.net>
+  Copyright 2011-2016 David Robillard <http://drobilla.net>
 
   Permission to use, copy, modify, and/or distribute this software for any
   purpose with or without fee is hereby granted, provided that the above
@@ -197,17 +197,15 @@ serd_node_new_uri(const SerdURI* uri, const SerdURI* base, SerdURI* out)
 		serd_uri_resolve(uri, base, &abs_uri);
 	}
 
-	const size_t len = serd_uri_string_length(&abs_uri);
-	uint8_t*     buf = (uint8_t*)malloc(len + 1);
-
-	SerdNode node = { buf, len, len, 0, SERD_URI };  // FIXME: UTF-8
-
+	const size_t len        = serd_uri_string_length(&abs_uri);
+	uint8_t*     buf        = (uint8_t*)malloc(len + 1);
+	SerdNode     node       = { buf, 0, 0, 0, SERD_URI };
 	uint8_t*     ptr        = buf;
 	const size_t actual_len = serd_uri_serialise(&abs_uri, string_sink, &ptr);
 
 	buf[actual_len] = '\0';
 	node.n_bytes    = actual_len;
-	node.n_chars    = actual_len;
+	node.n_chars    = serd_strlen(buf, NULL, NULL);
 
 	if (out) {
 		serd_uri_parse(buf, out);  // TODO: cleverly avoid double parse

src/string.c
@@ -1,5 +1,5 @@
 /*
-  Copyright 2011-2012 David Robillard <http://drobilla.net>
+  Copyright 2011-2016 David Robillard <http://drobilla.net>
 
   Permission to use, copy, modify, and/or distribute this software for any
   purpose with or without fee is hereby granted, provided that the above
@@ -40,24 +40,27 @@ SERD_API
 size_t
 serd_strlen(const uint8_t* str, size_t* n_bytes, SerdNodeFlags* flags)
 {
-	size_t n_chars = 0;
-	size_t i       = 0;
-	*flags         = 0;
+	size_t        n_chars = 0;
+	size_t        i       = 0;
+	SerdNodeFlags f       = 0;
 	for (; str[i]; ++i) {
 		if ((str[i] & 0xC0) != 0x80) {
 			// Does not start with `10', start of a new character
 			++n_chars;
 			switch (str[i]) {
 			case '\r': case '\n':
-				*flags |= SERD_HAS_NEWLINE;
+				f |= SERD_HAS_NEWLINE;
 				break;
 			case '"':
-				*flags |= SERD_HAS_QUOTE;
+				f |= SERD_HAS_QUOTE;
 			}
 		}
 	}
 	if (n_bytes) {
 		*n_bytes = i;
+	}
+	if (flags) {
+		*flags = f;
 	}
 	return n_chars;
 }

wscript
@@ -11,7 +11,7 @@ import waflib.extras.autowaf as autowaf
 # major increment <=> incompatible changes
 # minor increment <=> compatible changes (additions)
 # micro increment <=> no interface changes
-SERD_VERSION       = '0.22.3'
+SERD_VERSION       = '0.22.4'
 SERD_MAJOR_VERSION = '0'
 
 # Mandatory waf variables

commit b5d531a26d2b56108e1228e908c54620fbbc736b

Fix memory error
Introduced in 443f470383dc3acba0fde7b705e8ff81a7c49595

src/env.c
@@ -245,8 +245,9 @@ serd_env_expand_node(const SerdEnv*  env,
 		}
 		const size_t len = prefix.len + suffix.len;
 		uint8_t*     buf = (uint8_t*)malloc(len + 1);
-		SerdNode     ret = { buf, len, serd_strlen(buf, NULL, NULL), 0, SERD_URI };
-		snprintf((char*)buf, ret.n_bytes + 1, "%s%s", prefix.buf, suffix.buf);
+		SerdNode     ret = { buf, len, 0, 0, SERD_URI };
+		snprintf((char*)buf, len + 1, "%s%s", prefix.buf, suffix.buf);
+		ret.n_chars = serd_strlen(buf, NULL, NULL);
 		return ret;
 	}
 	case SERD_URI: {