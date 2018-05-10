3028c5eae2c1b2d3462b14e6a3dacba2b3a0914a
David Robillard <d@drobilla.net>
2018-05-10 20:14:24 +0200

    Move error handling to world

---
+++ src/world_internal.h
@@ -0,0 +1,25 @@
+// Copyright 2011-2020 David Robillard <d@drobilla.net>
+// SPDX-License-Identifier: ISC
+
+#ifndef SERD_SRC_WORLD_INTERNAL_H
+#define SERD_SRC_WORLD_INTERNAL_H
+
+#include <serd/error.h>
+#include <serd/status.h>
+#include <serd/world.h>
+
+#include <stdarg.h>
+
+SerdStatus
+serd_world_error(const SerdWorld* world, const SerdError* e);
+
+SerdStatus
+serd_world_errorf(const SerdWorld* world, SerdStatus st, const char* fmt, ...);
+
+SerdStatus
+serd_world_verrorf(const SerdWorld* world,
+                   SerdStatus       st,
+                   const char*      fmt,
+                   va_list          args);
+
+#endif // SERD_SRC_WORLD_INTERNAL_H
