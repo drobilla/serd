e4bc43b327c5324f857fe4a6fc28ed71381cfbdc
David Robillard <d@drobilla.net>
2018-05-13 00:47:58 +0200

    Remove datatype and language from reader context

--- src/reader.h
+++ src/reader.h
@@ -28,8 +28,6 @@ typedef struct {
   SerdNode*           subject;
   SerdNode*           predicate;
   SerdNode*           object;
-  SerdNode*           datatype;
-  SerdNode*           lang;
   SerdStatementFlags* flags;
 } ReadContext;
 
