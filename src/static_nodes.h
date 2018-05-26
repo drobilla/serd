8ebbb5709aac971cd6f41f7d412a4252d27bc5e7
David Robillard <d@drobilla.net>
2018-05-26 11:49:43 +0200

    Add debug checks for node padding

--- src/static_nodes.h
+++ src/static_nodes.h
@@ -11,7 +11,7 @@
 
 typedef struct StaticNode {
   SerdNode node;
-  char     buf[sizeof(NS_XSD "base64Binary")];
+  char     buf[sizeof(NS_XSD "base64Binary") + sizeof(SerdNode)];
 } StaticNode;
 
 #define DEFINE_XSD_NODE(name)                 \
