57b747f30b782b01067e1ad9139af19d8834a5a2
David Robillard <d@drobilla.net>
2018-05-27 15:32:12 +0200

    Move syntax name/extension utilities to public API

--- include/serd/syntax.h
+++ include/serd/syntax.h
@@ -6,6 +6,8 @@
 
 #include "serd/attributes.h"
 
+#include <stdbool.h>
+
 SERD_BEGIN_DECLS
 
 /**
@@ -14,13 +16,39 @@ SERD_BEGIN_DECLS
    @{
 */
 
-/// RDF syntax type
+/// Syntax supported by serd
 typedef enum {
   SERD_TURTLE   = 1U, ///< Terse triples http://www.w3.org/TR/turtle
   SERD_NTRIPLES = 2U, ///< Line-based triples http://www.w3.org/TR/n-triples/
   SERD_NQUADS   = 3U, ///< Line-based quads http://www.w3.org/TR/n-quads/
   SERD_TRIG     = 4U, ///< Terse quads http://www.w3.org/TR/trig/
 } SerdSyntax;
+
+/**
+   Get a syntax by name.
+
+   Case-insensitive, supports "Turtle", "NTriples", "NQuads", and "TriG".  Zero
+   is returned if the name is not recognized.
+*/
+SERD_PURE_API SerdSyntax
+serd_syntax_by_name(const char* SERD_NONNULL name);
+
+/**
+   Guess a syntax from a filename.
+
+   This uses the file extension to guess the syntax of a file.  Zero is
+   returned if the extension is not recognized.
+*/
+SERD_PURE_API SerdSyntax
+serd_guess_syntax(const char* SERD_NONNULL filename);
+
+/**
+   Return whether a syntax can represent multiple graphs in one document.
+
+   @return True for #SERD_NQUADS and #SERD_TRIG, false otherwise.
+*/
+SERD_CONST_API bool
+serd_syntax_has_graphs(SerdSyntax syntax);
 
 /**
    @}
