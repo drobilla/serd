b6644eb9fbc5968b65eb26d705a12c27c1707753
David Robillard <d@drobilla.net>
2018-05-27 15:32:12 +0200

    Move syntax name/extension utilities to public API

--- src/serdi.c
+++ src/serdi.c
@@ -2,10 +2,8 @@
 // SPDX-License-Identifier: ISC
 
 #include "serd_config.h"
-#include "string_utils.h"
 #include "system.h"
 
-#include "serd/attributes.h"
 #include "serd/env.h"
 #include "serd/error.h"
 #include "serd/node.h"
@@ -28,51 +26,9 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 #define SERDI_ERROR(msg) fprintf(stderr, "serdi: " msg)
 #define SERDI_ERRORF(fmt, ...) fprintf(stderr, "serdi: " fmt, __VA_ARGS__)
-
-typedef struct {
-  SerdSyntax  syntax;
-  const char* name;
-  const char* extension;
-} Syntax;
-
-static const Syntax syntaxes[] = {{SERD_TURTLE, "turtle", ".ttl"},
-                                  {SERD_NTRIPLES, "ntriples", ".nt"},
-                                  {SERD_NQUADS, "nquads", ".nq"},
-                                  {SERD_TRIG, "trig", ".trig"},
-                                  {(SerdSyntax)0, NULL, NULL}};
-
-static SerdSyntax
-get_syntax(const char* const name)
-{
-  for (const Syntax* s = syntaxes; s->name; ++s) {
-    if (!serd_strncasecmp(s->name, name, strlen(name))) {
-      return s->syntax;
-    }
-  }
-
-  SERDI_ERRORF("unknown syntax '%s'\n", name);
-  return (SerdSyntax)0;
-}
-
-static SERD_PURE_FUNC
-SerdSyntax
-guess_syntax(const char* const filename)
-{
-  const char* ext = strrchr(filename, '.');
-  if (ext) {
-    for (const Syntax* s = syntaxes; s->name; ++s) {
-      if (!serd_strncasecmp(s->extension, ext, strlen(ext))) {
-        return s->syntax;
-      }
-    }
-  }
-
-  return (SerdSyntax)0;
-}
 
 static int
 print_version(void)
@@ -219,7 +175,7 @@ main(int argc, char** argv)
           return missing_arg(prog, 'i');
         }
 
-        if (!(input_syntax = get_syntax(argv[a]))) {
+        if (!(input_syntax = serd_syntax_by_name(argv[a]))) {
           return print_usage(prog, true);
         }
         break;
@@ -241,7 +197,7 @@ main(int argc, char** argv)
           return missing_arg(prog, 'o');
         }
 
-        if (!(output_syntax = get_syntax(argv[a]))) {
+        if (!(output_syntax = serd_syntax_by_name(argv[a]))) {
           return print_usage(prog, true);
         }
         break;
@@ -278,15 +234,13 @@ main(int argc, char** argv)
 
   const char* input = argv[a++];
 
-  if (!input_syntax && !(input_syntax = guess_syntax(input))) {
+  if (!input_syntax && !(input_syntax = serd_guess_syntax(input))) {
     input_syntax = SERD_TRIG;
   }
 
+  const bool input_has_graphs = serd_syntax_has_graphs(input_syntax);
   if (!output_syntax) {
-    output_syntax =
-      ((input_syntax == SERD_TURTLE || input_syntax == SERD_NTRIPLES)
-         ? SERD_NTRIPLES
-         : SERD_NQUADS);
+    output_syntax = input_has_graphs ? SERD_NQUADS : SERD_NTRIPLES;
   }
 
   const SerdWriterFlags writer_flags =
@@ -294,7 +248,7 @@ main(int argc, char** argv)
 
   SerdNode* base = NULL;
   if (a < argc) { // Base URI given on command line
-    base = serd_new_uri(serd_string((const char*)argv[a]));
+    base = serd_new_uri(serd_string(argv[a]));
   } else if (!from_string && !from_stdin) { // Use input file URI
     base = serd_new_file_uri(serd_string(input), serd_empty_string());
   }
