2f2e4850652358d580b47cabc04b5e279272323e
David Robillard <d@drobilla.net>
2018-06-15 09:26:17 -0400

    Bring read/write interface closer to C standard

--- src/node.c
+++ src/node.c
@@ -46,7 +46,6 @@ static const size_t serd_node_align = 2 * sizeof(uint64_t);
 static SerdNode*
 serd_new_from_uri(SerdURIView uri, SerdURIView base);
 
-SERD_PURE_FUNC
 static size_t
 serd_uri_string_length(const SerdURIView* const uri)
 {
@@ -67,12 +66,15 @@ serd_uri_string_length(const SerdURIView* const uri)
 }
 
 static size_t
-string_sink(const void* const buf, const size_t len, void* const stream)
+string_sink(const void* const buf,
+            const size_t      size,
+            const size_t      nmemb,
+            void* const       stream)
 {
   char** ptr = (char**)stream;
-  memcpy(*ptr, buf, len);
-  *ptr += len;
-  return len;
+  memcpy(*ptr, buf, size * nmemb);
+  *ptr += size * nmemb;
+  return nmemb;
 }
 
 static size_t
@@ -376,16 +378,16 @@ serd_new_file_uri(const SerdStringView path, const SerdStringView hostname)
   SerdBuffer buffer = {uri, uri_len};
   for (size_t i = 0; i < path.len; ++i) {
     if (is_windows && path.buf[i] == '\\') {
-      serd_buffer_sink("/", 1, &buffer);
+      serd_buffer_sink("/", 1, 1, &buffer);
     } else if (path.buf[i] == '%') {
-      serd_buffer_sink("%%", 2, &buffer);
+      serd_buffer_sink("%%", 1, 2, &buffer);
     } else if (is_uri_path_char(path.buf[i])) {
-      serd_buffer_sink(path.buf + i, 1, &buffer);
+      serd_buffer_sink(path.buf + i, 1, 1, &buffer);
     } else {
       char escape_str[10] = {'%', 0, 0, 0, 0, 0, 0, 0, 0, 0};
       snprintf(
         escape_str + 1, sizeof(escape_str) - 1, "%X", (unsigned)path.buf[i]);
-      serd_buffer_sink(escape_str, 3, &buffer);
+      serd_buffer_sink(escape_str, 1, 3, &buffer);
     }
   }
   serd_buffer_sink_finish(&buffer);
