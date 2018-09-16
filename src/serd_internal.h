443d0288884e27024f899bbef5d5612c0f314e04
David Robillard <d@drobilla.net>
2018-09-16 22:46:17 +0200

    Fix const casts

--- src/serd_internal.h
+++ src/serd_internal.h
@@ -393,7 +393,7 @@ serd_strncasecmp(const char* s1, const char* s2, size_t n)
 {
 	for (; n > 0 && *s2; s1++, s2++, --n) {
 		if (toupper(*s1) != toupper(*s2)) {
-			return ((*(uint8_t*)s1 < *(uint8_t*)s2) ? -1 : +1);
+			return ((*(const uint8_t*)s1 < *(const uint8_t*)s2) ? -1 : +1);
 		}
 	}
 	return 0;
