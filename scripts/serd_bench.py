9d8a889f456ca87b113ffe2bfec36a5e1b8f478e
David Robillard <d@drobilla.net>
2018-09-30 13:23:52 +0200

    Add model to benchmarks

--- scripts/serd_bench.py
+++ scripts/serd_bench.py
@@ -46,8 +46,8 @@ def gen(sp2b_dir, n_min, n_max, step):
 
 def write_header(results, progs):
     "Write the header line for TSV output"
-    results.write("n")
-    for prog in progs:
+    results.write("n\tserdi_stream\tserdi_model")
+    for prog in progs[2:]:
         results.write("\t" + os.path.basename(prog.split()[0]))
     results.write("\n")
 
@@ -150,13 +150,15 @@ def run(progs, n_min, n_max, step):
                 cmd = "/usr/bin/time -v " + prog + " " + filename(n)
                 with open(filename(n) + ".out", "w") as out:
                     sys.stderr.write(cmd + "\n")
-                    proc = subprocess.Popen(
+                    proc = subprocess.run(
                         cmd.split(), stdout=out, stderr=subprocess.PIPE
                     )
 
-                    time, memory = parse_time(proc.communicate()[1].decode())
+                    time, memory = parse_time(proc.stderr.decode())
                     rows["time"] += ["%.07f" % time]
-                    rows["throughput"] += ["%d" % (n / time)]
+                    rows["throughput"] += (
+                        ["%d" % (n / time)] if time > 0.0 else ["0"]
+                    )
                     rows["memory"] += [str(memory)]
 
             # Write rows to output files
@@ -228,7 +230,11 @@ example:
 
     args = ap.parse_args(sys.argv[1:])
 
-    progs = ["serdi -f -i turtle -o turtle"] + args.run
+    progs = [
+        "serdi -i turtle -o turtle",
+        "serdi -m -i turtle -o turtle",
+    ] + args.run
+
     min_n = int(args.max / 10)
     max_n = args.max
     step = min_n
