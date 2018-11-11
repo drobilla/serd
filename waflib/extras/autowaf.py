ba99a2957bd2567d4c1c626c8710f32295962c3e
David Robillard <d@drobilla.net>
2018-11-11 12:14:05 +0100

    Make waf exit with non-zero status when test suite fails

--- waflib/extras/autowaf.py
+++ waflib/extras/autowaf.py
@@ -818,6 +818,7 @@ def post_test(ctx, appname, dirs=['src'], remove=['*boost*', 'c++*']):
     Logs.pprint('GREEN', '[  PASSED  ] %d tests' % passed_tests)
     if failed_tests > 0:
         Logs.pprint('RED', '[  FAILED  ] %d tests' % failed_tests)
+        sys.exit(1)
     Logs.pprint('', '')
 
     top_level = (len(ctx.stack_path) > 1)
