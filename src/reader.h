06c8b143a2896cc1328f5988778c573ac931bddc
David Robillard <d@drobilla.net>
2018-12-29 19:56:13 -0500

    Rename SerdType to SerdNodeType
    The old name will be even more ambiguous with the increased scope.

--- src/reader.h
+++ src/reader.h
@@ -72,14 +72,17 @@ SerdStatus
 r_err_char(SerdReader* reader, const char* kind, int c);
 
 Ref
-push_node_padded(SerdReader* reader,
-                 size_t      maxlen,
-                 SerdType    type,
-                 const char* str,
-                 size_t      n_bytes);
+push_node_padded(SerdReader*  reader,
+                 size_t       maxlen,
+                 SerdNodeType type,
+                 const char*  str,
+                 size_t       n_bytes);
 
 Ref
-push_node(SerdReader* reader, SerdType type, const char* str, size_t n_bytes);
+push_node(SerdReader*  reader,
+          SerdNodeType type,
+          const char*  str,
+          size_t       n_bytes);
 
 SERD_PURE_FUNC size_t
 genid_size(const SerdReader* reader);
