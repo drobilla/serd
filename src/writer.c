8b3f3fc1b6311cb79f3836d666bff6596f7ef726
David Robillard <d@drobilla.net>
2019-03-10 18:00:41 +0100

    Reset writer to empty when finished to avoid spurious blank lines

--- src/writer.c
+++ src/writer.c
@@ -1051,6 +1051,7 @@ serd_writer_finish(SerdWriter* writer)
   free_context(writer);
   writer->indent  = 0;
   writer->context = WRITE_CONTEXT_NULL;
+  writer->empty   = true;
   return SERD_SUCCESS;
 }
 
