45dfaba50f02a65e8b83692538fb0aeb6d4dc5ef
David Robillard <d@drobilla.net>
2019-03-10 13:06:33 +0100

    Factor out writing top level separator

--- src/writer.c
+++ src/writer.c
@@ -488,6 +488,14 @@ write_newline(SerdWriter* writer)
   }
 }
 
+static void
+write_top_level_sep(SerdWriter* writer)
+{
+  if (!writer->empty) {
+    write_newline(writer);
+  }
+}
+
 static bool
 write_sep(SerdWriter* writer, const Sep sep)
 {
@@ -881,10 +889,7 @@ serd_writer_write_statement(SerdWriter* const          writer,
       write_sep(writer, SEP_GRAPH_END);
     }
 
-    if (!writer->empty) {
-      write_newline(writer); // Blank line between top level items
-    }
-
+    write_top_level_sep(writer);
     reset_context(writer, true);
 
     if (graph) {
@@ -934,9 +939,7 @@ serd_writer_write_statement(SerdWriter* const          writer,
       if (ctx(writer, SERD_SUBJECT)) {
         write_sep(writer, SEP_END_S); // Terminate last subject
       }
-      if (!writer->empty) {
-        write_newline(writer); // Blank line between top level items
-      }
+      write_top_level_sep(writer);
     }
 
     if (serd_stack_is_empty(&writer->anon_stack)) {
