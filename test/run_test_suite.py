cf88670f8edaac10630597ac3425e020c2a8512c
David Robillard <d@drobilla.net>
2019-05-03 20:18:43 +0200

    Add input base URI option

--- test/run_test_suite.py
+++ test/run_test_suite.py
@@ -85,8 +85,9 @@ def test_thru(
             "foo",
             "-w",
             out_path,
-            path,
+            "-I",
             base_uri,
+            path,
         ]
     )
 
@@ -103,8 +104,9 @@ def test_thru(
             "-w",
             thru_path,
             "-a",
-            out_path,
+            "-I",
             base_uri,
+            out_path,
         ]
     )
 
@@ -168,7 +170,7 @@ def _load_rdf(filename, base_uri, command_prefix):
     model = {}
     instances = {}
 
-    cmd = command_prefix + [filename, base_uri]
+    cmd = command_prefix + ["-I", base_uri, filename]
     proc = subprocess.run(cmd, capture_output=True, check=True)
     for line in proc.stdout.splitlines():
         matches = re.match(
@@ -275,9 +277,15 @@ def test_suite(
             test_name = os.path.basename(test_uri_path)
             test_path = os.path.join(test_dir, test_name)
 
-            command = (
-                command_prefix + ["-a", "-o", osyntax] + [test_path, test_uri]
-            )
+            command = command_prefix + [
+                "-a",
+                "-o",
+                osyntax,
+                "-I",
+                test_uri,
+                test_path,
+            ]
+
             command_string = " ".join(shlex.quote(c) for c in command)
             out_filename = os.path.join(out_test_dir, test_name + ".out")
 
