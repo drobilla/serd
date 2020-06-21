fc889eb79b7b3057e6bc74c697ebc29347b8f7fb
David Robillard <d@drobilla.net>
2020-06-21 18:19:48 +0200

    Add serd_node_from_syntax() and serd_node_to_syntax()

--- test/test_node.c
+++ test/test_node.c
@@ -398,7 +398,7 @@ test_node_equals(void)
 }
 
 static void
-test_node_from_string(void)
+test_node_from_syntax(void)
 {
   SerdNode* const hello = serd_new_string(SERD_STRING("hello\""));
   assert(serd_node_length(hello) == 6);
@@ -515,7 +515,7 @@ main(void)
   test_get_integer();
   test_blob_to_node();
   test_node_equals();
-  test_node_from_string();
+  test_node_from_syntax();
   test_node_from_substring();
   test_simple_node();
   test_literal();
