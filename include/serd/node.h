cb322cbc5e5276d812633ac7973f53187a1af195
David Robillard <d@drobilla.net>
2020-11-13 11:22:18 +0100

    Shorten node constructor names

--- include/serd/node.h
+++ include/serd/node.h
@@ -113,19 +113,19 @@ serd_node_from_substring(SerdNodeType              type,
                          const char* SERD_NULLABLE str,
                          size_t                    len);
 
-/// Simple wrapper for serd_node_new_uri() to resolve a URI node
+/// Create a new URI node
 SERD_API
 SerdNode
-serd_node_new_uri_from_node(const SerdNode* SERD_NONNULL     uri_node,
-                            const SerdURIView* SERD_NULLABLE base,
-                            SerdURIView* SERD_NULLABLE       out);
+serd_new_uri_from_node(const SerdNode* SERD_NONNULL     uri_node,
+                       const SerdURIView* SERD_NULLABLE base,
+                       SerdURIView* SERD_NULLABLE       out);
 
-/// Simple wrapper for serd_node_new_uri() to resolve a URI string
+/// Simple wrapper for serd_new_uri() to resolve a URI string
 SERD_API
 SerdNode
-serd_node_new_uri_from_string(const char* SERD_NULLABLE        str,
-                              const SerdURIView* SERD_NULLABLE base,
-                              SerdURIView* SERD_NULLABLE       out);
+serd_new_uri_from_string(const char* SERD_NULLABLE        str,
+                         const SerdURIView* SERD_NULLABLE base,
+                         SerdURIView* SERD_NULLABLE       out);
 
 /**
    Create a new file URI node from a file system path and optional hostname.
@@ -138,9 +138,9 @@ serd_node_new_uri_from_string(const char* SERD_NULLABLE        str,
 */
 SERD_API
 SerdNode
-serd_node_new_file_uri(const char* SERD_NONNULL   path,
-                       const char* SERD_NULLABLE  hostname,
-                       SerdURIView* SERD_NULLABLE out);
+serd_new_file_uri(const char* SERD_NONNULL   path,
+                  const char* SERD_NULLABLE  hostname,
+                  SerdURIView* SERD_NULLABLE out);
 
 /**
    Create a new node by serialising `uri` into a new string.
@@ -154,9 +154,9 @@ serd_node_new_file_uri(const char* SERD_NONNULL   path,
 */
 SERD_API
 SerdNode
-serd_node_new_uri(const SerdURIView* SERD_NONNULL  uri,
-                  const SerdURIView* SERD_NULLABLE base,
-                  SerdURIView* SERD_NULLABLE       out);
+serd_new_uri(const SerdURIView* SERD_NONNULL  uri,
+             const SerdURIView* SERD_NULLABLE base,
+             SerdURIView* SERD_NULLABLE       out);
 
 /**
    Create a new node by serialising `uri` into a new relative URI.
@@ -172,10 +172,10 @@ serd_node_new_uri(const SerdURIView* SERD_NONNULL  uri,
 */
 SERD_API
 SerdNode
-serd_node_new_relative_uri(const SerdURIView* SERD_NONNULL  uri,
-                           const SerdURIView* SERD_NULLABLE base,
-                           const SerdURIView* SERD_NULLABLE root,
-                           SerdURIView* SERD_NULLABLE       out);
+serd_new_relative_uri(const SerdURIView* SERD_NONNULL  uri,
+                      const SerdURIView* SERD_NULLABLE base,
+                      const SerdURIView* SERD_NULLABLE root,
+                      SerdURIView* SERD_NULLABLE       out);
 
 /**
    Create a new node by serialising `d` into an xsd:decimal string.
@@ -194,12 +194,12 @@ serd_node_new_relative_uri(const SerdURIView* SERD_NONNULL  uri,
 */
 SERD_API
 SerdNode
-serd_node_new_decimal(double d, unsigned frac_digits);
+serd_new_decimal(double d, unsigned frac_digits);
 
 /// Create a new node by serialising `i` into an xsd:integer string
 SERD_API
 SerdNode
-serd_node_new_integer(int64_t i);
+serd_new_integer(int64_t i);
 
 /**
    Create a node by serialising `buf` into an xsd:base64Binary string.
@@ -213,7 +213,7 @@ serd_node_new_integer(int64_t i);
 */
 SERD_API
 SerdNode
-serd_node_new_blob(const void* SERD_NONNULL buf, size_t size, bool wrap_lines);
+serd_new_blob(const void* SERD_NONNULL buf, size_t size, bool wrap_lines);
 
 /**
    Make a deep copy of `node`.
