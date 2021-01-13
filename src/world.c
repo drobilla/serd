1867b4d93fb03c4ec6c74b34e48dd12f11f12561
David Robillard <d@drobilla.net>
2021-01-13 12:08:30 +0100

    Add SerdCursor

--- src/world.c
+++ src/world.c
@@ -18,6 +18,7 @@
 
 #include "world.h"
 
+#include "cursor.h"
 #include "node.h"
 #include "serd_config.h"
 
@@ -59,10 +60,13 @@ serd_world_error(const SerdWorld* const world, const SerdError* const e)
   if (world->error_func) {
     world->error_func(world->error_handle, e);
   } else {
-    if (e->filename) {
-      fprintf(stderr, "error: %s:%u:%u: ", e->filename, e->line, e->col);
-    } else {
-      fprintf(stderr, "error: ");
+    fprintf(stderr, "error: ");
+    if (e->cursor) {
+      fprintf(stderr,
+              "%s:%u:%u: ",
+              serd_node_string(e->cursor->file),
+              e->cursor->line,
+              e->cursor->col);
     }
     vfprintf(stderr, e->fmt, *e->args);
   }
@@ -77,7 +81,7 @@ serd_world_errorf(const SerdWorld* const world,
 {
   va_list args;
   va_start(args, fmt);
-  const SerdError e = {st, NULL, 0, 0, fmt, &args};
+  const SerdError e = {st, NULL, fmt, &args};
   serd_world_error(world, &e);
   va_end(args);
   return st;
