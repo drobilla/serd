5819afbac00e703b5f36efa12a9422fd1bb713f7
David Robillard <d@drobilla.net>
2021-02-20 16:50:11 -0500

    Zero node padding before passing to reader sinks

--- src/n3.c
+++ src/n3.c
@@ -1189,7 +1189,7 @@ read_object(SerdReader* const  reader,
     o->flags = flags;
   }
 
-  if (!ret && emit && simple) {
+  if (!ret && emit && simple && o) {
     ret = emit_statement(reader, *ctx, o);
   } else if (!ret && !emit) {
     ctx->object = o;
@@ -1428,6 +1428,7 @@ read_base(SerdReader* const reader, const bool sparql, const bool token)
 
   SerdNode* uri = NULL;
   TRY(st, read_IRIREF(reader, &uri));
+  serd_node_zero_pad(uri);
   TRY(st, serd_sink_write_base(reader->sink, uri));
 
   read_ws_star(reader);
@@ -1468,6 +1469,8 @@ read_prefixID(SerdReader* const reader, const bool sparql, const bool token)
   SerdNode* uri = NULL;
   TRY(st, read_IRIREF(reader, &uri));
 
+  serd_node_zero_pad(name);
+  serd_node_zero_pad(uri);
   st = serd_sink_write_prefix(reader->sink, name, uri);
 
   if (!sparql) {
