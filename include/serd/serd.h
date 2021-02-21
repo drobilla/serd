8caf46a41d80e68503594fcaf4bcf5aa614688f8
David Robillard <d@drobilla.net>
2021-02-20 20:10:57 -0500

    Simplify node expansion

--- include/serd/serd.h
+++ include/serd/serd.h
@@ -1021,7 +1021,7 @@ serd_env_qualify(const SerdEnv* SERD_NULLABLE env,
                  const SerdNode* SERD_NONNULL uri);
 
 /**
-   Expand `node`, transforming CURIEs into URIs.
+   Expand `node`, transforming CURIEs and URI references into absolute URIs.
 
    If `node` is a relative URI reference, it is expanded to a full URI if
    possible.  If `node` is a literal, its datatype is expanded if necessary.
