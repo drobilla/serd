e7ecca722964720f97b1a165898778ba867f00bb
David Robillard <d@drobilla.net>
2021-02-25 12:12:15 -0500

    Split up serd_internal.h

--- src/n3.c
+++ src/n3.c
@@ -2,9 +2,9 @@
 // SPDX-License-Identifier: ISC
 
 #include "byte_source.h"
+#include "namespaces.h"
 #include "node.h"
 #include "reader.h"
-#include "serd_internal.h"
 #include "stack.h"
 #include "string_utils.h"
 #include "try.h"
--- src/system.h
+++ src/system.h
@@ -9,6 +9,8 @@
 #include <stdint.h>
 #include <stdio.h>
 
+#define SERD_PAGE_SIZE 4096
+
 /// Write the message for a system error code (like errno) to a buffer
 int
 serd_system_strerror(int errnum, char* buf, size_t buflen);
