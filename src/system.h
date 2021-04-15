e38f6fa48a3d3a5cafbdc87d3bd8748b01182cf9
David Robillard <d@drobilla.net>
2021-04-15 10:52:11 -0400

    Align node allocations

--- src/system.h
+++ src/system.h
@@ -29,6 +29,11 @@ serd_fopen(const char* path, const char* mode);
 SERD_MALLOC_FUNC void*
 serd_malloc_aligned(size_t alignment, size_t size);
 
+/// Allocate a zeroed buffer aligned to `alignment` bytes
+SERD_MALLOC_FUNC
+void*
+serd_calloc_aligned(size_t alignment, size_t size);
+
 /// Allocate an aligned buffer for I/O
 SERD_MALLOC_FUNC void*
 serd_allocate_buffer(size_t size);
