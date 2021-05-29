624accde94a105213dc3562606c638d9620e7a72
David Robillard <d@drobilla.net>
2021-05-29 19:59:33 -0400

    Fix include order

--- src/reader.c
+++ src/reader.c
@@ -14,8 +14,9 @@
   OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 */
 
-#include "byte_source.h"
 #include "reader.h"
+
+#include "byte_source.h"
 #include "stack.h"
 #include "system.h"
 
