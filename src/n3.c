f4c4c0072db99565da33822e02bbca0c3943668a
David Robillard <d@drobilla.net>
2021-07-11 11:39:01 -0400

    Fix hang when skipping an error at EOF when lax parsing

--- src/n3.c
+++ src/n3.c
@@ -1685,7 +1685,7 @@ read_n3_statement(SerdReader* const reader)
 static void
 skip_until(SerdReader* const reader, const uint8_t byte)
 {
-  for (int c = 0; (c = peek_byte(reader)) && c != byte;) {
+  for (int c = 0; (c = peek_byte(reader)) && c != byte && c != EOF;) {
     eat_byte_safe(reader, c);
   }
 }
