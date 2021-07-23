c655e256ba628baa8e8fc48fd98df9b25fa63426
David Robillard <d@drobilla.net>
2021-07-22 23:39:39 -0400

    Expose low-level node construction API

--- src/node.h
+++ src/node.h
@@ -7,7 +7,9 @@
 #include "exess/exess.h"
 #include "serd/attributes.h"
 #include "serd/node.h"
+#include "serd/string_view.h"
 
+#include <assert.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -20,18 +22,61 @@ struct SerdNodeImpl {
 
 static const size_t serd_node_align = 2 * sizeof(uint64_t);
 
+#if SIZE_MAX == UINT64_MAX
+
+static inline size_t
+serd_node_pad_length(const size_t n_bytes)
+{
+  const size_t align = sizeof(SerdNode);
+
+  assert((align & (align - 1U)) == 0U);
+
+  return (n_bytes + align + 2U) & ~(align - 1U);
+}
+
+#else
+
+static inline size_t
+serd_node_pad_length(const size_t n_bytes)
+{
+  const size_t pad  = sizeof(SerdNode) - (n_bytes + 2) % sizeof(SerdNode);
+  const size_t size = n_bytes + 2 + pad;
+  assert(size % sizeof(SerdNode) == 0);
+  return size;
+}
+
+#endif
+
+SERD_CONST_FUNC
 static inline char* SERD_NONNULL
 serd_node_buffer(SerdNode* SERD_NONNULL node)
 {
   return (char*)(node + 1);
 }
 
+SERD_PURE_FUNC
 static inline const char* SERD_NONNULL
 serd_node_buffer_c(const SerdNode* SERD_NONNULL node)
 {
   return (const char*)(node + 1);
 }
 
+SERD_PURE_FUNC
+static inline SerdNode* SERD_NONNULL
+serd_node_meta(SerdNode* const SERD_NONNULL node)
+{
+  return node + 1 + (serd_node_pad_length(node->length) / sizeof(SerdNode));
+}
+
+SERD_PURE_FUNC
+static inline const SerdNode* SERD_NONNULL
+serd_node_meta_c(const SerdNode* const SERD_NONNULL node)
+{
+  assert(node->flags & (SERD_HAS_DATATYPE | SERD_HAS_LANGUAGE));
+  return node + 1 + (serd_node_pad_length(node->length) / sizeof(SerdNode));
+}
+
+SERD_CONST_FUNC
 static inline const char* SERD_NONNULL
 serd_node_string_i(const SerdNode* const SERD_NONNULL node)
 {
@@ -45,8 +90,17 @@ serd_node_pattern_match(const SerdNode* SERD_NULLABLE a,
   return !a || !b || serd_node_equals(a, b);
 }
 
+SERD_PURE_FUNC
+bool
+is_langtag(SerdStringView string);
+
+SERD_MALLOC_FUNC
 SerdNode* SERD_ALLOCATED
-serd_node_malloc(size_t length, SerdNodeFlags flags, SerdNodeType type);
+serd_node_malloc(size_t size);
+
+SERD_MALLOC_FUNC
+SerdNode* SERD_ALLOCATED
+serd_node_try_malloc(SerdWriteResult result);
 
 void
 serd_node_set(SerdNode* SERD_NULLABLE* SERD_NONNULL dst,
@@ -54,7 +108,7 @@ serd_node_set(SerdNode* SERD_NULLABLE* SERD_NONNULL dst,
 
 SERD_PURE_FUNC
 size_t
-serd_node_total_size(const SerdNode* SERD_NULLABLE node);
+serd_node_total_size(const SerdNode* SERD_NONNULL node);
 
 void
 serd_node_zero_pad(SerdNode* SERD_NONNULL node);
--- test/test_writer.c
+++ test/test_writer.c
@@ -102,7 +102,7 @@ test_writer_stack_overflow(void)
   SerdNode* const s = serd_new_uri(serd_string("http://example.org/s"));
   SerdNode* const p = serd_new_uri(serd_string("http://example.org/p"));
 
-  SerdNode*  o  = serd_new_blank(serd_string("blank"));
+  SerdNode*  o  = serd_new_token(SERD_BLANK, serd_string("blank"));
   SerdStatus st = serd_sink_write(sink, SERD_ANON_O, s, p, o, NULL);
   assert(!st);
 
@@ -111,7 +111,7 @@ test_writer_stack_overflow(void)
     char buf[1024];
     snprintf(buf, sizeof(buf), "b%u", i);
 
-    SerdNode* next_o = serd_new_blank(serd_string(buf));
+    SerdNode* next_o = serd_new_token(SERD_BLANK, serd_string(buf));
 
     st = serd_sink_write(sink, SERD_ANON_O, o, p, next_o, NULL);
 
