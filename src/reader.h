e2aa4c6f3167175b8af7c35e1760fe713bd4aad5
David Robillard <d@drobilla.net>
2021-07-24 21:08:10 -0400

    Be precise about the meaning of node "length" and "size"

--- src/reader.h
+++ src/reader.h
@@ -62,7 +62,7 @@ r_err(SerdReader* reader, SerdStatus st, const char* fmt, ...);
 
 SerdNode*
 push_node_padded(SerdReader*  reader,
-                 size_t       maxlen,
+                 size_t       max_length,
                  SerdNodeType type,
                  const char*  str,
                  size_t       length);
@@ -75,7 +75,7 @@ push_node(SerdReader*  reader,
 
 SERD_PURE_FUNC
 size_t
-genid_size(const SerdReader* reader);
+genid_length(const SerdReader* reader);
 
 SERD_PURE_FUNC
 bool
