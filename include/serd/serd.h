b9f15bf650656464fd640130d06b3f65c4b391af
David Robillard <d@drobilla.net>
2021-07-27 19:01:45 -0400

    Add statement filtering

--- include/serd/serd.h
+++ include/serd/serd.h
@@ -1895,6 +1895,41 @@ serd_canon_new(const SerdWorld* SERD_NULLABLE world,
 
 /**
    @}
+   @defgroup serd_filter Filter
+   @{
+*/
+
+/**
+   Return a new sink that filters out statements that do not match a pattern.
+
+   The returned sink acts like `target` in all respects, except that some
+   statements may be dropped.
+
+   @param target The target sink to pass the filtered data to.
+
+   @param subject The optional subject of the filter pattern.
+
+   @param predicate The optional predicate of the filter pattern.
+
+   @param object The optional object of the filter pattern.
+
+   @param graph The optional graph of the filter pattern.
+
+   @param inclusive If true, then only statements that match the pattern are
+   passed through.  Otherwise, only statements that do *not* match the pattern
+   are passed through.
+*/
+SERD_API
+SerdSink* SERD_ALLOCATED
+serd_filter_new(const SerdSink* SERD_NONNULL  target,
+                const SerdNode* SERD_NULLABLE subject,
+                const SerdNode* SERD_NULLABLE predicate,
+                const SerdNode* SERD_NULLABLE object,
+                const SerdNode* SERD_NULLABLE graph,
+                bool                          inclusive);
+
+/**
+   @}
    @defgroup serd_env Environment
    @{
 */
