3f6bf147f44a1a01d13290eab8505d2ecb3e72f1
David Robillard <d@drobilla.net>
2021-08-01 17:23:24 -0400

    Make serd_writer_set_root_uri() take a string view
    This is generally more convenient, and the node was just being copied anyway.

--- src/writer.c
+++ src/writer.c
@@ -1435,7 +1435,7 @@ serd_writer_set_base_uri(SerdWriter* writer, const SerdNode* uri)
 }
 
 SerdStatus
-serd_writer_set_root_uri(SerdWriter* writer, const SerdNode* uri)
+serd_writer_set_root_uri(SerdWriter* writer, const ZixStringView uri)
 {
   assert(writer);
 
@@ -1443,8 +1443,8 @@ serd_writer_set_root_uri(SerdWriter* writer, const SerdNode* uri)
   writer->root_node = NULL;
   writer->root_uri  = SERD_URI_NULL;
 
-  if (uri) {
-    writer->root_node = serd_node_copy(uri);
+  if (uri.length) {
+    writer->root_node = serd_new_uri(uri);
     writer->root_uri  = serd_node_uri_view(writer->root_node);
   }
 
