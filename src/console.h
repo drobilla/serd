9114522ff58671c5e3f91a181feb6c208bcd3233
David Robillard <d@drobilla.net>
2021-08-02 11:32:31 -0400

    Factor out opening input files

--- src/console.h
+++ src/console.h
@@ -1,6 +1,7 @@
 // Copyright 2021 David Robillard <d@drobilla.net>
 // SPDX-License-Identifier: ISC
 
+#include "serd/byte_source.h"
 #include "serd/env.h"
 #include "serd/status.h"
 
@@ -11,6 +12,9 @@ serd_set_stream_utf8_mode(FILE* stream);
 
 int
 serd_print_version(const char* program);
+
+SerdByteSource*
+serd_open_tool_input(const char* filename, size_t page_size);
 
 SerdStatus
 serd_set_base_uri_from_path(SerdEnv* env, const char* path);
