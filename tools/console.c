b794e57548b27d1f22e10414da912722dacbe6bc
David Robillard <d@drobilla.net>
2021-08-02 11:56:54 -0400

    Factor out setting base URI from a path

--- tools/console.c
+++ tools/console.c
@@ -4,6 +4,9 @@
 #include "console.h"
 
 #include "serd/serd.h"
+#include "zix/allocator.h"
+#include "zix/filesystem.h"
+#include "zix/string_view.h"
 
 #ifdef _WIN32
 #  ifdef _MSC_VER
@@ -41,6 +44,24 @@ serd_print_version(const char* const program)
          "There is NO WARRANTY, to the extent permitted by law.\n");
 
   return 0;
+}
+
+SerdStatus
+serd_set_base_uri_from_path(SerdEnv* const env, const char* const path)
+{
+  char* const input_path = zix_canonical_path(NULL, path);
+  if (!input_path) {
+    return SERD_BAD_ARG;
+  }
+
+  SerdNode* const file_uri =
+    serd_new_file_uri(NULL, zix_string(input_path), zix_empty_string());
+
+  serd_env_set_base_uri(env, serd_node_string_view(file_uri));
+  serd_node_free(NULL, file_uri);
+  zix_free(NULL, input_path);
+
+  return SERD_SUCCESS;
 }
 
 /// Wrapper for getc that is compatible with SerdReadFunc but faster than fread
