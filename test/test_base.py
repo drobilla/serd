dc4e980edc1e8e879a76574cbe6d2505cde03e7b
David Robillard <d@drobilla.net>
2021-08-01 20:09:38 -0400

    Collapse input and output options into single flags

--- test/test_base.py
+++ test/test_base.py
@@ -10,7 +10,7 @@
 import serd_test_util as util
 
 args = util.wrapper_args(__doc__)
-command = [args.tool, "-B", "http://example.org", "-i", "turtle", "-"]
+command = [args.tool, "-B", "http://example.org", "-I", "turtle", "-"]
 
 IN_DOC = "<s> <p> <o> ."
 OUT_DOC = "<{0}s> <{0}p> <{0}o> .".format("http://example.org/")
