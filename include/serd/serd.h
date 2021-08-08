dd6a95a8b6fde278c0a374f085119065683d04eb
David Robillard <d@drobilla.net>
2021-08-08 13:47:10 -0400

    Add version constants to public header

--- include/serd/serd.h
+++ include/serd/serd.h
@@ -77,6 +77,39 @@ extern "C" {
 */
 
 /**
+   @defgroup serd_version Version
+
+   Serd uses a single [semantic version number](https://semver.org) which
+   reflects changes to the C library ABI.
+
+   @{
+*/
+
+/**
+   The major version number of the serd library.
+
+   Semver: Increments when incompatible API changes are made.
+*/
+#define SERD_MAJOR_VERSION 1
+
+/**
+   The minor version number of the serd library.
+
+   Semver: Increments when functionality is added in a backwards compatible
+   manner.
+*/
+#define SERD_MINOR_VERSION 1
+
+/**
+   The micro version number of the serd library.
+
+   Semver: Increments when changes are made that do not affect the API, such as
+   performance improvements or bug fixes.
+*/
+#define SERD_MICRO_VERSION 1
+
+/**
+   @}
    @defgroup serd_string_view String View
    @{
 */
