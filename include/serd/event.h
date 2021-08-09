8db718c0532edccf3664fe9e4ad61209e3d8a57a
David Robillard <d@drobilla.net>
2021-08-09 18:28:35 -0400

    Preserve anonymous graph syntax in TriG

--- include/serd/event.h
+++ include/serd/event.h
@@ -32,12 +32,13 @@ typedef enum {
 typedef enum {
   SERD_EMPTY_S = 1U << 0U, ///< Empty blank node subject
   SERD_EMPTY_O = 1U << 1U, ///< Empty blank node object
-  SERD_ANON_S  = 1U << 2U, ///< Start of anonymous subject
-  SERD_ANON_O  = 1U << 3U, ///< Start of anonymous object
-  SERD_LIST_S  = 1U << 4U, ///< Start of list subject
-  SERD_LIST_O  = 1U << 5U, ///< Start of list object
-  SERD_TERSE_S = 1U << 6U, ///< Start of terse subject
-  SERD_TERSE_O = 1U << 7U, ///< Start of terse object
+  SERD_EMPTY_G = 1U << 2U, ///< Empty blank node graph
+  SERD_ANON_S  = 1U << 3U, ///< Start of anonymous subject
+  SERD_ANON_O  = 1U << 4U, ///< Start of anonymous object
+  SERD_LIST_S  = 1U << 5U, ///< Start of list subject
+  SERD_LIST_O  = 1U << 6U, ///< Start of list object
+  SERD_TERSE_S = 1U << 7U, ///< Start of terse subject
+  SERD_TERSE_O = 1U << 8U, ///< Start of terse object
 } SerdStatementEventFlag;
 
 /// Bitwise OR of SerdSinkStatementFlag values
