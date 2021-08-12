6b3b2c02a7e4c635abedc9de4d34d17210f03b79
David Robillard <d@drobilla.net>
2021-08-12 13:42:25 -0400

    Replace serdi with more fine-grained tools
    Especially with the new functionality, the complexity of the command-line
    interface alone was really becoming unmanageable.  The serdi implementation
    also had the highest cyclomatic complexity of the entire codebase by a huge
    margin.
    
    So, take a page from the Unix philosophy and split serdi into several more
    finely-honed tools that can be freely composed.  Though there is still
    unfortunately quite a bit of option overlap between them due to the common
    details of reading RDF, I think the resulting tools are a lot easier to
    understand, both from a user and a developer perspective.

--- test/test_quiet.py
+++ test/test_quiet.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-"""Test serdi quiet option."""
+"""Test quiet command-line option."""
 
 import argparse
 import sys
@@ -9,12 +9,12 @@ import subprocess
 
 parser = argparse.ArgumentParser(description=__doc__)
 
-parser.add_argument("--serdi", default="./serdi", help="path to serdi")
+parser.add_argument("--tool", default="tools/serd-pipe", help="executable")
 parser.add_argument("--wrapper", default="", help="executable wrapper")
 parser.add_argument("input", help="invalid input file")
 
 args = parser.parse_args(sys.argv[1:])
-command = shlex.split(args.wrapper) + [args.serdi, "-q", args.input]
+command = shlex.split(args.wrapper) + [args.tool, "-q", args.input]
 proc = subprocess.run(command, check=False, capture_output=True)
 
 assert proc.returncode != 0
