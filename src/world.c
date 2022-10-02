85aafa2465aecb416e6b48c39a523c158c2514f7
David Robillard <d@drobilla.net>
2022-10-02 18:19:49 -0400

    Add serd_world_get_blank()

--- src/world.c
+++ src/world.c
@@ -3,8 +3,11 @@
 
 #include "world.h"
 
+#include "node.h"
 #include "serd_config.h"
 #include "system.h"
+
+#include "serd/string_view.h"
 
 #if defined(USE_POSIX_FADVISE)
 #  include <fcntl.h>
@@ -14,6 +17,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 FILE*
 serd_world_fopen(SerdWorld* world, const char* path, const char* mode)
@@ -68,13 +72,36 @@ serd_world_errorf(const SerdWorld* const world,
 SerdWorld*
 serd_world_new(void)
 {
-  return (SerdWorld*)calloc(1, sizeof(SerdWorld));
+  SerdWorld* world = (SerdWorld*)calloc(1, sizeof(SerdWorld));
+
+  world->blank_node = serd_new_blank(serd_string("b00000000000"));
+
+  return world;
 }
 
 void
 serd_world_free(SerdWorld* const world)
 {
-  free(world);
+  if (world) {
+    serd_node_free(world->blank_node);
+    free(world);
+  }
+}
+
+const SerdNode*
+serd_world_get_blank(SerdWorld* const world)
+{
+#define BLANK_CHARS 12
+
+  char* buf = serd_node_buffer(world->blank_node);
+  memset(buf, 0, BLANK_CHARS + 1);
+
+  world->blank_node->length =
+    (size_t)snprintf(buf, BLANK_CHARS + 1, "b%u", ++world->next_blank_id);
+
+  return world->blank_node;
+
+#undef BLANK_CHARS
 }
 
 void
