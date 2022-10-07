94e66b9793932483b5b0f5f67a16c4c16bd4665c
David Robillard <d@drobilla.net>
2022-10-07 13:25:56 -0400

    Override pkg-config dependency within meson

--- src/serd_config.h
+++ src/serd_config.h
@@ -16,7 +16,7 @@
 #define SERD_CONFIG_H
 
 // Define version unconditionally so a warning will catch a mismatch
-#define SERD_VERSION "0.30.16"
+#define SERD_VERSION "0.30.17"
 
 #if !defined(SERD_NO_DEFAULT_CONFIG)
 
