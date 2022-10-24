2c1e022ebbb2619b6e497a360480294bb950e20e
David Robillard <d@drobilla.net>
2022-10-23 21:48:38 -0400

    Test header for warnings more strictly

---
+++ test/headers/test_headers.c
@@ -0,0 +1,11 @@
+// Copyright 2022 David Robillard <d@drobilla.net>
+// SPDX-License-Identifier: ISC
+
+#include "serd/serd.h" // IWYU pragma: keep
+
+SERD_CONST_FUNC
+int
+main(void)
+{
+  return 0;
+}
