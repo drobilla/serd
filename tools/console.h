0a10a251e7502bf3907b358a7c5fcd8110a5a383
David Robillard <d@drobilla.net>
2023-03-29 23:34:40 -0400

    Add support for reading multiple files at once

--- tools/console.h
+++ tools/console.h
@@ -4,6 +4,8 @@
 #ifndef SERD_TOOLS_CONSOLE_H
 #define SERD_TOOLS_CONSOLE_H
 
+#include "serd/input_stream.h"
+
 #include <stdio.h>
 
 void
@@ -11,5 +13,8 @@ serd_set_stream_utf8_mode(FILE* stream);
 
 int
 serd_print_version(const char* program);
+
+SerdInputStream
+serd_open_tool_input(const char* filename);
 
 #endif // SERD_TOOLS_CONSOLE_H
