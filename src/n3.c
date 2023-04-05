82e8963e3d76e56afac8b3f72126556b9c36643f
David Robillard <d@drobilla.net>
2023-04-05 17:26:04 -0400

    Remove support for reading Turtle named inline nodes extension

--- src/n3.c
+++ src/n3.c
@@ -1003,21 +1003,6 @@ read_BLANK_NODE_LABEL(SerdReader* const reader,
   return SERD_SUCCESS;
 }
 
-static Ref
-read_blankName(SerdReader* const reader)
-{
-  if (skip_byte(reader, '=') || eat_byte_check(reader, '=') != '=') {
-    r_err(reader, SERD_ERR_BAD_SYNTAX, "expected '='\n");
-    return 0;
-  }
-
-  Ref  subject = 0;
-  bool ate_dot = false;
-  read_ws_star(reader);
-  read_iri(reader, &subject, &ate_dot);
-  return subject;
-}
-
 static SerdStatus
 read_anon(SerdReader* const reader,
           ReadContext       ctx,
@@ -1033,11 +1018,6 @@ read_anon(SerdReader* const reader,
     *ctx.flags |= empty ? SERD_EMPTY_S : SERD_ANON_S_BEGIN;
   } else {
     *ctx.flags |= empty ? SERD_EMPTY_O : SERD_ANON_O_BEGIN;
-    if (peek_delim(reader, '=')) {
-      if (!(*dest = read_blankName(reader)) || !eat_delim(reader, ';')) {
-        return SERD_ERR_BAD_SYNTAX;
-      }
-    }
   }
 
   if (!*dest) {
