231fdf17f4f03f4af3c5c79bbc21635598c3491e
David Robillard <d@drobilla.net>
2023-12-01 18:31:30 -0500

    Replace -b and -e options with a block size option
    This is more powerful, and reduces the number of command line options that
    almost nobody needs to care about.

--- src/system.h
+++ src/system.h
@@ -4,29 +4,12 @@
 #ifndef SERD_SRC_SYSTEM_H
 #define SERD_SRC_SYSTEM_H
 
-#include <stdint.h>
-#include <stdio.h>
+#include <stddef.h>
 
 #define SERD_PAGE_SIZE 4096
 
 /// Write the message for a system error code (like errno) to a buffer
 int
 serd_system_strerror(int errnum, char* buf, size_t buflen);
-
-/// Wrapper for getc that is compatible with SerdReadFunc
-static inline size_t
-serd_file_read_byte(void* buf, size_t size, size_t nmemb, void* stream)
-{
-  (void)size;
-  (void)nmemb;
-
-  const int c = getc((FILE*)stream);
-  if (c == EOF) {
-    *((uint8_t*)buf) = 0;
-    return 0;
-  }
-  *((uint8_t*)buf) = (uint8_t)c;
-  return 1;
-}
 
 #endif // SERD_SRC_SYSTEM_H
