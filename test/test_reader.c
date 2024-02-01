3dd91acb3bd0ad0f161692caa23420f62a779f8a
David Robillard <d@drobilla.net>
2024-02-01 15:30:41 -0500

    Use a simple type-safe stack in writer

--- test/test_reader.c
+++ test/test_reader.c
@@ -56,7 +56,7 @@ test_sink(void* const handle, const SerdEvent* const event)
 static void
 test_read_string(void)
 {
-  static const SerdLimits limits = {512};
+  static const SerdLimits limits = {512, 1};
 
   SerdWorld* const world = serd_world_new();
   serd_world_set_limits(world, limits);
@@ -134,7 +134,7 @@ eof_test_error(void* stream)
 static void
 test_read_eof_by_page(const char* const path)
 {
-  static const SerdLimits limits = {1024};
+  static const SerdLimits limits = {1024, 1};
 
   FILE* const f = fopen(path, "w+b");
   assert(f);
@@ -171,7 +171,7 @@ test_read_eof_by_page(const char* const path)
 static void
 test_read_eof_by_byte(void)
 {
-  static const SerdLimits limits = {1024};
+  static const SerdLimits limits = {1024, 1};
 
   SerdWorld* const world = serd_world_new();
   serd_world_set_limits(world, limits);
@@ -199,7 +199,7 @@ test_read_eof_by_byte(void)
 static void
 test_read_flat_chunks(const char* const path, const SerdSyntax syntax)
 {
-  static const SerdLimits limits = {1024};
+  static const SerdLimits limits = {1024, 1};
   static const char       null   = 0;
 
   FILE* const f = fopen(path, "w+b");
@@ -289,7 +289,7 @@ test_read_flat_chunks(const char* const path, const SerdSyntax syntax)
 static void
 test_read_abbrev_chunks(const char* const path, const SerdSyntax syntax)
 {
-  static const SerdLimits limits = {1024};
+  static const SerdLimits limits = {1024, 2};
   static const char       null   = 0;
 
   FILE* const f = fopen(path, "w+b");
@@ -409,7 +409,7 @@ empty_test_error(void* stream)
 static void
 test_read_empty(void)
 {
-  static const SerdLimits limits = {512};
+  static const SerdLimits limits = {512, 1};
 
   SerdWorld* const world = serd_world_new();
   serd_world_set_limits(world, limits);
