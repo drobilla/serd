e2e4c7034a792dd17fc7b888e6084f2ff7b2d420
David Robillard <d@drobilla.net>
2024-02-06 07:57:19 -0500

    Refuse to read or write invalid IRI characters
    The grammar rules for IRIs exclude a set of special characters (which are also
    excluded by the URI specification).  However, the UCHAR escape allows these
    characters to be encoded.
    
    This is a strange intersection of URI and RDF grammars, but these characters
    are special here because the URI specification says they can't exist in a URI
    without being percent-encoded.  NTriples happens to have a general-purpose
    escape syntax here, but I see no practical reason to support using it as a
    hostile mechanism to encode invalid URIs.  So, refuse to read these characters
    in URIs, escaped or otherwise, and refuse to write "URIs" that contain them.
    
    I imagine this was the intent of the specification, I was just working too
    literally from the grammar.  If the ability to parse URIs with such invalid
    characters is needed, tolerance here can be grafted on to lax mode.  Until
    then, I'll stick with the safe and simple approach, which is sufficient for all
    the W3C tests.
    
    Testing the writer error cases requires custom code now, because it's
    impossible to read a URI that contains these characters from a file.
    
    Towards properly implementing percent-encoding and decoding, where this becomes
    critical for the character '%' in particular.

--- src/read_ntriples.c
+++ src/read_ntriples.c
@@ -102,7 +102,7 @@ SerdStatus
 read_IRIREF_suffix(SerdReader* const reader, SerdNode* const node)
 {
   SerdStatus st   = SERD_SUCCESS;
-  uint32_t   code = 0U;
+  int        code = 0;
 
   while (st <= SERD_FAILURE) {
     const int c = peek_byte(reader);
@@ -116,11 +116,10 @@ read_IRIREF_suffix(SerdReader* const reader, SerdNode* const node)
       st = read_utf8_continuation(reader, node, (uint8_t)c);
     } else if (c == '\\') {
       if (!(st = read_UCHAR(reader, node, &code)) &&
-          (code == ' ' || code == '<' || code == '>')) {
-        st = r_err_char(reader, "IRI", (int)code);
+          (code == '%' || !is_IRIREF(code))) {
+        st = r_err_char(reader, "IRI", code);
       }
-    } else if (c > 0x20 && c != '"' && c != '<' && c != '^' && c != '`' &&
-               c != '{' && c != '|' && c != '}') {
+    } else if (c > 0 && c < 0x7F && is_IRIREF(c)) {
       st = push_byte(reader, node, c);
     } else {
       st = r_err_char(reader, "IRI", c);
@@ -165,7 +164,7 @@ SerdStatus
 read_string_escape(SerdReader* const reader, SerdNode* const ref)
 {
   const SerdStatus st   = read_ECHAR(reader, ref);
-  uint32_t         code = 0U;
+  int              code = 0;
 
   return st == SERD_FAILURE ? read_UCHAR(reader, ref, &code) : st;
 }
@@ -354,7 +353,7 @@ utf8_from_codepoint(uint8_t* const out, const uint32_t code)
 SerdStatus
 read_UCHAR(SerdReader* const reader,
            SerdNode* const   node,
-           uint32_t* const   code_point)
+           int* const        code_point)
 {
   SerdStatus st = SERD_SUCCESS;
 
@@ -384,7 +383,8 @@ read_UCHAR(SerdReader* const reader,
                            : push_bytes(reader, node, replacement_char, 3));
   }
 
-  *code_point = code;
+  assert(code <= 0x0010FFFFU);
+  *code_point = (int)code;
   return push_bytes(reader, node, buf, size);
 }
 
