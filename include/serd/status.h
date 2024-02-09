d38f438a75243cf0b6e9cadb377a990f74842361
David Robillard <d@drobilla.net>
2024-02-08 22:24:20 -0500

    Organize SerdStatus more conveniently

--- include/serd/status.h
+++ include/serd/status.h
@@ -22,20 +22,22 @@ typedef enum {
   SERD_NO_DATA,  ///< Missing input
   SERD_NO_SPACE, ///< Insufficient space
 
-  SERD_UNKNOWN_ERROR, ///< Unknown error
+  SERD_UNKNOWN_ERROR = 8, ///< Unknown error
 
-  SERD_BAD_SYNTAX,  ///< Invalid syntax
-  SERD_BAD_ARG,     ///< Invalid argument
-  SERD_BAD_LABEL,   ///< Encountered clashing blank node label
+  SERD_BAD_ALLOC,  ///< Memory allocation failed
+  SERD_BAD_READ,   ///< Error reading from file
+  SERD_BAD_WRITE,  ///< Error writing to file
+  SERD_BAD_STREAM, ///< File or stream error
+  SERD_BAD_STACK,  ///< Stack overflow
+  SERD_BAD_CALL,   ///< Bad function call
+  SERD_BAD_ARG,    ///< Bad function argument
+  SERD_BAD_EVENT,  ///< Corrupt event in stream
+
+  SERD_BAD_SYNTAX = 32, ///< Invalid syntax
+
+  SERD_BAD_LABEL,   ///< Clashing blank node label
   SERD_BAD_CURIE,   ///< Invalid CURIE or unknown namespace prefix
-  SERD_BAD_ALLOC,   ///< Memory allocation failed
-  SERD_BAD_READ,    ///< Error reading from file
-  SERD_BAD_WRITE,   ///< Error writing to file
-  SERD_BAD_STREAM,  ///< File or stream error
-  SERD_BAD_STACK,   ///< Stack overflow
   SERD_BAD_TEXT,    ///< Invalid text encoding
-  SERD_BAD_CALL,    ///< Invalid call
-  SERD_BAD_EVENT,   ///< Invalid event in stream
   SERD_BAD_URI,     ///< Invalid or unresolved URI
   SERD_BAD_DATA,    ///< Invalid data
   SERD_BAD_LITERAL, ///< Invalid literal
