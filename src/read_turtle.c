690b564a7990f04fd24b3a734fb583024a44b542
David Robillard <d@drobilla.net>
2024-03-10 10:25:33 -0400

    Simplify input stream API

--- src/read_turtle.c
+++ src/read_turtle.c
@@ -50,7 +50,7 @@ read_whitespace(SerdReader* const reader)
   case '\n':
   case '\r':
   case ' ':
-    return serd_byte_source_advance(&reader->source);
+    return serd_byte_source_advance(reader->source);
   case '#':
     return read_comment(reader);
   default:
@@ -1013,7 +1013,7 @@ read_turtle_statement(SerdReader* const reader)
 SerdStatus
 read_turtleDoc(SerdReader* const reader)
 {
-  while (!reader->source.eof) {
+  while (!reader->source->eof) {
     const size_t     orig_stack_size = reader->stack.size;
     const SerdStatus st              = read_turtle_statement(reader);
 
