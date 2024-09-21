77427d7d91514b03e5c3e0ab38bb03c9e162f5f3
David Robillard <d@drobilla.net>
2024-09-21 08:23:44 -0400

    [WIP] Bump major version

--- src/serd_config.h
+++ src/serd_config.h
@@ -36,7 +36,7 @@
 #define SERD_SRC_SERD_CONFIG_H
 
 // Define version unconditionally so a warning will catch a mismatch
-#define SERD_VERSION "0.32.9"
+#define SERD_VERSION "1.1.1"
 
 #ifndef SERD_NO_DEFAULT_CONFIG
 
