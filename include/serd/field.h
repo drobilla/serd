621d163dd6962a7e6b5d1b6345dfc2bc5cbabdca
David Robillard <d@drobilla.net>
2024-09-22 21:15:31 -0400

    Add serd_field_supports()

--- include/serd/field.h
+++ include/serd/field.h
@@ -5,6 +5,10 @@
 #define SERD_FIELD_H
 
 #include <serd/attributes.h>
+#include <serd/node_type.h>
+#include <zix/attributes.h>
+
+#include <stdbool.h>
 
 SERD_BEGIN_DECLS
 
@@ -21,6 +25,10 @@ typedef enum {
   SERD_OBJECT    = 2U, ///< Object ("value")
   SERD_GRAPH     = 3U, ///< Graph ("context")
 } SerdField;
+
+/// Return true if a field can hold the given node type
+SERD_CONST_API ZIX_NODISCARD bool
+serd_field_supports(SerdField field, SerdNodeType type);
 
 /**
    @}
