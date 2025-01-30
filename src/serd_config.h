4ef30fd5e0f175a4f8c79be49fb15a4fc6676517
David Robillard <d@drobilla.net>
2025-01-30 14:33:01 -0500

    Remove project and version number from man page OS field
    The mandoc documentation suggests leaving this blank.  Although I still think
    it's nicer for third-party projects to put their own versions here (as many GNU
    utilities do, for example), it's a hassle in practice to maintain the version
    over time, and I'm not sure what to do about the date in that case.
    
    The version is passed to the mandoc command instead for generating the HTML man
    pages, so those are unchanged.

--- src/serd_config.h
+++ src/serd_config.h
@@ -36,7 +36,7 @@
 #define SERD_SRC_SERD_CONFIG_H
 
 // Define version unconditionally so a warning will catch a mismatch
-#define SERD_VERSION "0.32.4"
+#define SERD_VERSION "0.32.5"
 
 #if !defined(SERD_NO_DEFAULT_CONFIG)
 
